393546cdfbcd310e9082ad7fc4cf4c8193984f32
Kendrick Coleman <kendrickcoleman@gmail.com>
2015-09-17 15:28:41 -0400

    completed weave plugin
    created weave plugin and tested on ubuntu
    
    Signed-off-by: Kendrick Coleman <kendrickcoleman@gmail.com>

--- libmachine/extension/weave.go
+++ libmachine/extension/weave.go
@@ -2,6 +2,7 @@ package extension
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/docker/machine/libmachine/provision"
 	"github.com/docker/machine/log"
@@ -36,31 +37,41 @@ func (extension *WeaveExtension) Install(provisioner provision.Provisioner, host
 		weaveVersion = extInfo.version
 	}
 
-	provisioner.SSHCommand("sudo curl -L git.io/weave -o /usr/local/bin/weave")
-	provisioner.SSHCommand("sudo chmod a+x /usr/local/bin/weave")
-	switch hostInfo.OsID {
-	case "ubuntu", "debian":
-		log.Debugf("WEAVE: found supported OS: %s", hostInfo.OsID)
+	log.Debugf("%s: downloading", strings.ToUpper(extInfo.name))
+	if _, err := provisioner.SSHCommand("sudo curl -L git.io/weave -o /usr/local/bin/weave"); err != nil {
+		return err
+	}
+	log.Debugf("%s: setting permissions", strings.ToUpper(extInfo.name))
+	if _, err := provisioner.SSHCommand("sudo chmod a+x /usr/local/bin/weave"); err != nil {
+		return err
+	}
 
-	case "centos", "redhat":
-		log.Debugf("WEAVE: found supported OS: %s", hostInfo.OsID)
+	switch hostInfo.OsID {
+	case "ubuntu", "debian", "centos", "redhat", "coreos":
+		log.Debugf("%s: found supported OS: %s", strings.ToUpper(extInfo.name), hostInfo.OsID)
 	default:
-		return fmt.Errorf("WEAVE not supported on: %s", hostInfo.OsID)
+		return fmt.Errorf("%s not supported on: %s", strings.ToUpper(extInfo.name), hostInfo.OsID)
 	}
 
 	if extInfo.params != nil {
-		weaveKvLoop(provisioner, hostInfo, extInfo)
+		weaveLaunch(provisioner, extInfo)
+	} else {
+		log.Debugf("%s: launching first weave node", strings.ToUpper(extInfo.name))
+		if _, err := provisioner.SSHCommand("sudo weave launch"); err != nil {
+			return err
+		}
 	}
-	provisioner.SSHCommand("sudo weave launch-dns")
-	provisioner.SSHCommand("sudo weave launch-proxy")
+
 	return nil
 }
 
-func weaveKvLoop(provisioner provision.Provisioner, hostInfo *ExtensionParams, extInfo *ExtensionInfo) error {
+func weaveLaunch(provisioner provision.Provisioner, extInfo *ExtensionInfo) error {
 	for k, v := range extInfo.params {
-		if k != hostInfo.Hostname {
-			log.Debugf("WEAVE: Launching Peer Connection to: %s", k)
-			provisioner.SSHCommand(fmt.Sprintf("sudo weave launch %s", v))
+		if k == "peer" {
+			log.Debugf("%s: Launching Peer Connection to: %s", strings.ToUpper(extInfo.name), v)
+			if _, err := provisioner.SSHCommand(fmt.Sprintf("sudo weave launch %s", v)); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
